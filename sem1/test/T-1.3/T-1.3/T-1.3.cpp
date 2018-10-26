e17de2dfd69e451871d05544f6f2311281c14070
NadiaSob <sobo_na@mail.ru>
2018-10-26 19:06:00 +0300

    added task 3 from the test

---
+++ sem1/test/T-1.3/T-1.3/T-1.3.cpp
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	FILE * file = fopen("file.txt", "r");
+	if (!file)
+	{
+		printf("File is not found");
+		return 1;
+	}
+	char string[100] = {};
+	char *estr = new char[100];
+	while (!feof(file))
+	{
+		estr = fgets(string, 100, file);
+		for (int i = 0; string[i] != '\0'; ++i)
+		{
+			if (string[i] == ';')
+			{
+				for (int j = i + 1; string[j] != '\0'; ++j)
+				{
+					printf("%c", string[j]);
+				}
+			}
+		}
+	}
+	fclose(file);
+	return 0;
+}
