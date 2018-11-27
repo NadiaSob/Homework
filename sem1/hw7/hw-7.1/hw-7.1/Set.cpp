4a4c5fe18ac0648a6b531400db630822338e71cf
NadiaSob <sobo_na@mail.ru>
2018-11-28 01:53:14 +0300

    added task 7.1

---
+++ sem1/hw7/hw-7.1/hw-7.1/Set.cpp
@@ -0,0 +1,277 @@
+#include "Set.h"
+
+struct Node;
+
+struct Set
+{
+	Node *head = nullptr;
+};
+
+struct Node
+{
+	int data = 0;
+	Node *leftChild = nullptr;
+	Node *rightChild = nullptr;
+};
+
+Set *createSet()
+{
+	return new Set;
+}
+
+bool isEmpty(Set *set)
+{
+	return set->head == nullptr;
+}
+
+bool add(Set *set, int const data)
+{
+	if (isEmpty(set))
+	{
+		set->head = new Node{ data, nullptr, nullptr };
+	}
+	else
+	{
+		if (!exists(set, data))
+		{
+			addNode(set->head, data);
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void addNode(Node *node, int data)
+{
+	if (node->data > data && node->leftChild != nullptr)
+	{
+		addNode(node->leftChild, data);
+	}
+	else if (node->data < data && node->rightChild != nullptr)
+	{
+		addNode(node->rightChild, data);
+	}
+	else
+	{
+		if (node->data > data)
+		{
+			node->leftChild = new Node{ data, nullptr, nullptr };
+		}
+		else
+		{
+			node->rightChild = new Node{ data, nullptr, nullptr };
+		}
+	}
+}
+
+bool deleteNode(Set *set, int const data)
+{
+	if (!exists(set, data))
+	{
+		return false;
+	}
+	deleteNodeRecursion(set->head, data);
+	return true;
+}
+
+void deleteNodeRecursion(Node *&current, int data)
+{
+	if (current->data > data)
+	{
+		deleteNodeRecursion(current->leftChild, data);
+	}
+	else if (current->data < data)
+	{
+		deleteNodeRecursion(current->rightChild, data);
+	}
+	else
+	{
+		if (current->leftChild == nullptr && current->rightChild == nullptr)
+		{
+			auto *temp = current;
+			current = nullptr;
+			delete temp;
+			return;
+		}
+		else if (current->leftChild == nullptr && current->rightChild != nullptr)
+		{
+			auto *temp = current;
+			current = current->rightChild;
+			delete temp;
+			return;
+		}
+		else if (current->leftChild != nullptr && current->rightChild == nullptr)
+		{
+			auto *temp = current;
+			current = current->leftChild;
+			delete temp;
+			return;
+		}
+		else
+		{
+			current->data = maximum(current);
+			deleteNodeRecursion(current->leftChild, current->data);
+		}
+	}
+}
+
+int maximum(Node *current)
+{
+	auto *temp = current->leftChild;
+	while (temp->rightChild != nullptr)
+	{
+		temp = temp->rightChild;
+	}
+	return temp->data;
+}
+
+bool exists(Set *set, int const data)
+{
+	if (isEmpty(set))
+	{
+		return false;
+	}
+	auto *temp = set->head;
+	while (true)
+	{
+		if (temp->data > data)
+		{
+			if (temp->leftChild != nullptr)
+			{
+				temp = temp->leftChild;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if (temp->data < data)
+		{
+			if (temp->rightChild != nullptr)
+			{
+				temp = temp->rightChild;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else
+		{
+			return true;
+		}
+	}
+}
+
+void deleteSet(Set *set)
+{
+	deleteSetRecursion(set->head);
+	delete set;
+}
+
+void deleteSetRecursion(Node *nodeToDelete)
+{
+	if (nodeToDelete != nullptr)
+	{
+		deleteSetRecursion(nodeToDelete->leftChild);
+		deleteSetRecursion(nodeToDelete->rightChild);
+		delete nodeToDelete;
+	}
+}
+
+void printSet(Set *set, int *array, bool InAscendingOrder)
+{
+	if (isEmpty(set))
+	{
+		return;
+	}
+	int count = 0;
+	if (InAscendingOrder)
+	{
+		printInAscendingOrder(set->head, array, count);
+	}
+	else
+	{
+		printInDescendingOrder(set->head, array, count);
+	}
+}
+
+void printInAscendingOrder(Node *nodeToPrint, int *array, int &count)
+{
+	if (nodeToPrint->leftChild != nullptr)
+	{
+		printInAscendingOrder(nodeToPrint->leftChild, array, count);
+	}
+	array[count] = nodeToPrint->data;
+	++count;
+
+	if (nodeToPrint->rightChild != nullptr)
+	{
+		printInAscendingOrder(nodeToPrint->rightChild, array, count);
+	}
+}
+
+void printInDescendingOrder(Node *nodeToPrint, int *array, int &count)
+{
+	if (nodeToPrint->rightChild != nullptr)
+	{
+		printInDescendingOrder(nodeToPrint->rightChild, array, count);
+	}
+	array[count] = nodeToPrint->data;
+	++count;
+
+	if (nodeToPrint->leftChild != nullptr)
+	{
+		printInDescendingOrder(nodeToPrint->leftChild, array, count);
+	}
+}
+
+bool test()
+{
+	Set *testSet = createSet();
+
+	add(testSet, 3);
+	add(testSet, 1);
+	add(testSet, 4);
+	add(testSet, 5);
+	add(testSet, 2);
+	add(testSet, 6);
+
+	deleteNode(testSet, 4);
+
+	if (!exists(testSet, 1) || !exists(testSet, 3) || exists(testSet, 4))
+	{
+		deleteSet(testSet);
+		return false;
+	}
+
+	int testArray[5]{};
+
+	printSet(testSet, testArray, 1);
+
+	for (int i = 0; i < 4; ++i)
+	{
+		if (testArray[i] > testArray[i + 1])
+		{
+			deleteSet(testSet);
+			return false;
+		}
+	}
+
+	printSet(testSet, testArray, 0);
+
+	for (int i = 0; i < 4; ++i)
+	{
+		if (testArray[i] < testArray[i + 1])
+		{
+			deleteSet(testSet);
+			return false;
+		}
+	}
+
+	deleteSet(testSet);
+	return true;
+}
---
+++ sem1/hw7/hw-7.1/hw-7.1/Set.h
@@ -0,0 +1,47 @@
+#pragma once
+
+struct Node;
+
+struct Set;
+
+//Creates new set
+Set *createSet();
+
+//Checks if set is empty
+bool isEmpty(Set *set);
+
+//Adds new node in set
+bool add(Set *set, int const data);
+
+//Used in function add
+void addNode(Node *node, int data);
+
+//Deletes node with given data
+bool deleteNode(Set *set, int const data);
+
+//Used in function deleteNode
+void deleteNodeRecursion(Node *&current, int data);
+
+//Finds maximum of the left subtree
+int maximum(Node *current);
+
+//Checkes if node with given data exists
+bool exists(Set *set, int const data);
+
+//Deletes set
+void deleteSet(Set *set);
+
+//Used in function deleteSet
+void deleteSetRecursion(Node *nodeToDelete);
+
+//Adds set elements in array
+void printSet(Set *set, int *array, bool InAscendingOrder);
+
+//Adds set elements in array in ascending order
+void printInAscendingOrder(Node *nodeToPrint, int *array, int &count);
+
+//Adds set elements in array in descending order
+void printInDescendingOrder(Node *nodeToPrint, int *array, int &count);
+
+//Tests set functions
+bool test();
