e47f7c726ea403138e00586881fe7d152bb567c3
NadiaSob <sobo_na@mail.ru>
2019-02-16 20:47:52 +0300

    added task 1.1

---
+++ sem2/hw1/hw1.1/hw1.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace hw1._1
+{
+    class Program
+    {
+        private static int Factorial(int number)
+        {
+            if (number <= 1)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number");
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Factorial of {number} is {Factorial(number)}");
+        }
+    }
+}
