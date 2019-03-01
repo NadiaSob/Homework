641f8655ba6ac54e831d0694330701adf6783806
NadiaSob <sobo_na@mail.ru>
2019-03-01 12:46:27 +0300

    Added task 2.1

---
+++ sem2/hw2/hw2.1/hw2.1/Commands.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace hw2._1
+{
+    class Commands
+    {
+        public void PrintCommands()
+        {
+            Console.WriteLine("Commands:");
+            Console.WriteLine("0 - exit");
+            Console.WriteLine("1 - add new element in list by its position");
+            Console.WriteLine("2 - delete element from list by its position");
+            Console.WriteLine("3 - get size of the list");
+            Console.WriteLine("4 - check if the list is empty");
+            Console.WriteLine("5 - get data of list's element by its position");
+            Console.WriteLine("6 - set data of list's element by its position");
+            Console.WriteLine("7 - print list");
+            Console.WriteLine("Enter the command");
+        }
+
+        bool IsCorrectPositionToAdd(List list, int position) => position > 0 && position <= list.size + 1;
+
+        bool IsCorrectPosition(List list, int position) => position > 0 && position <= list.size;
+
+        public void CommandExecution(List list, int command)
+        {
+            var data = 0;
+            var position = 0;
+            switch (command)
+            {
+                case 0:
+                    break;
+
+                case 1:
+                    Console.WriteLine("Enter the position of element in list");
+                    position = int.Parse(Console.ReadLine());
+                    if (!IsCorrectPositionToAdd(list, position))
+                    {
+                        Console.WriteLine("The position you entered is incorrect");
+                        break;
+                    }
+
+                    Console.WriteLine("Enter the data");
+                    data = int.Parse(Console.ReadLine());
+
+                    list.AddElement(position, data);
+                    break;
+
+                case 2:
+                    Console.WriteLine("Enter the position of element you'd like to delete");
+                    position = int.Parse(Console.ReadLine());
+                    if (!IsCorrectPosition(list, position))
+                    {
+                        Console.WriteLine("The position you entered is incorrect");
+                        break;
+                    }
+
+                    list.DeleteElement(position);
+                    break;
+
+                case 3:
+                    Console.WriteLine($"Size of the list is {list.size}");
+                    break;
+
+                case 4:
+                    if (list.IsEmpty())
+                    {
+                        Console.WriteLine("The list is empty");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The list is not empty");
+                    }
+                    break;
+
+                case 5:
+                    Console.WriteLine("Enter the position of element in list");
+                    position = int.Parse(Console.ReadLine());
+                    if (!IsCorrectPosition(list, position))
+                    {
+                        Console.WriteLine("The position you entered is incorrect");
+                        break;
+                    }
+
+                    Console.WriteLine($"Data of the element in position {position} is {list.GetData(position)}");
+                    break;
+
+                case 6:
+                    Console.WriteLine("Enter the position of element in list");
+                    position = int.Parse(Console.ReadLine());
+                    if (!IsCorrectPosition(list, position))
+                    {
+                        Console.WriteLine("The position you entered is incorrect");
+                        break;
+                    }
+
+                    Console.WriteLine("Enter the data");
+                    data = int.Parse(Console.ReadLine());
+
+                    list.SetData(position, data);
+                    break;
+
+                case 7:
+                    if (list.IsEmpty())
+                    {
+                        Console.WriteLine("The list is empty");
+                    }
+                    else
+                    {
+                        list.PrintList();
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("The command you entered is incorrect");
+                    break;
+            }
+            return;
+        }
+    }
+}
---
+++ sem2/hw2/hw2.1/hw2.1/List.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace hw2._1
+{
+    public class List
+    {
+        private class Node
+        {
+            public int data { get; set; }
+            public Node next { get; set; }
+
+            public Node(int data, Node next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+        }
+
+        private Node head;
+        public int size { get; private set; }
+
+        public List()
+        {
+        }
+
+        public bool IsEmpty() => head == null;
+
+        Node GetNode(int position)
+        {
+            Node current = head;
+            for (var i = 1; i < position; ++i)
+            {
+                current = current.next;
+            }
+            return current;
+        }
+
+        public void AddElement(int position, int data)
+        {
+            var newNode = new Node(data, null);
+
+            if (position == 1)
+            {
+                newNode.next = head;
+                head = newNode;
+            }
+            else
+            {
+                var current = GetNode(position - 1);
+                newNode.next = current.next;
+                current.next = newNode;
+            }
+            ++size;
+        }
+
+        public void DeleteElement(int position)
+        {
+            if (position == 1)
+            {
+                head = head.next;
+            }
+            else
+            {
+                Node current = GetNode(position - 1);
+                current.next = current.next.next;
+            }
+            --size;
+        }
+
+        public int GetData(int position)
+        {
+            Node node = GetNode(position);
+            return node.data;
+        }
+
+        public void SetData(int position, int data)
+        {
+            Node node = GetNode(position);
+            node.data = data;
+        }
+
+        public void PrintList()
+        {
+            Node current = head;
+            while(current != null)
+            {
+                Console.Write($"{current.data} ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
