0f36ff2ca073f1ade4969ab09b9dce8cede16ab7
NadiaSob <sobo_na@mail.ru>
2019-04-12 15:15:25 +0300

    Added test task

---
+++ sem2/tests/test1/test1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new PriorityQueue();
+            var commands = new Commands();
+            var command = 1;
+
+            while (command != 0)
+            {
+                commands.PrintCommands();
+                command = int.Parse(Console.ReadLine());
+                commands.DoCommand(queue, command);
+            }
+        }
+    }
+}
