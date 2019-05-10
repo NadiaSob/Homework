8f1893023e45cad3ad5cffff87c28a55e850b6c5
NadiaSob <sobo_na@mail.ru>
2019-05-10 15:17:43 +0300

    Added test2 task

---
+++ sem2/tests/test2/test2/SortedSet.Tests/SortedSetTest.cs
@@ -0,0 +1,57 @@
+﻿namespace SortedSet.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.Collections.Generic;
+    using test2;
+
+    [TestClass]
+    public class SortedSetTest
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            testSet = new SortedSet();
+        }
+
+        [TestMethod]
+        public void IsEmptyTest()
+        {
+            Assert.IsTrue(testSet.IsEmpty());
+        }
+
+        [TestMethod]
+        public void AddElementTest()
+        {
+            testSet.AddElement(new List<string>() { "abc", "def " });
+            Assert.IsFalse(testSet.IsEmpty());
+        }
+
+        [TestMethod]
+        public void AddFewElementsTest()
+        {
+            testSet.AddElement(new List<string>() { "one", "two", "three" });
+            testSet.AddElement(new List<string>() { "TestString" });
+            testSet.AddElement(new List<string>() { "lala", "la " });
+            Assert.IsFalse(testSet.IsEmpty());
+        }
+
+        [TestMethod]
+        public void SizeTest()
+        {
+            testSet.AddElement(new List<string>() { "a", "b" });
+            testSet.AddElement(new List<string>() { "d", "e" });
+            testSet.AddElement(new List<string>() { "fgh" });
+            Assert.AreEqual(3, testSet.Size);
+        }
+
+        [TestMethod]
+        public void SizeZeroTest()
+        {
+            Assert.IsTrue(testSet.IsEmpty());
+            Assert.AreEqual(0, testSet.Size);
+        }
+
+        private SortedSet testSet;
+    }
+}
---
+++ sem2/tests/test2/test2/test2/SortedSet.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    /// <summary>
+    /// Realisation of sorted set.
+    /// </summary>
+    public class SortedSet
+    {
+        public SortedSet()
+        {
+            comparer = new Comparer<List<string>>();
+            Size = 0;
+        }
+
+        private class Element
+        {
+            public List<string> Data { get; set; }
+            public Element Next { get; set; }
+            public int Position { get; set; }
+
+            public Element(List<string> data, Element next)
+            {
+                Data = data;
+                Next = next;
+            }
+        }
+
+        private Element head;
+
+        private readonly IComparer<List<string>> comparer;
+
+        /// <summary>
+        /// Number of elements in set.
+        /// </summary>
+        public int Size { get; private set; }
+
+        /// <summary>
+        /// Checks is set is empty.
+        /// </summary>
+        /// <returns>True if the set is empty, else false.</returns>
+        public bool IsEmpty() => head == null;
+
+        /// <summary>
+        /// Adds element in the correct set position.
+        /// </summary>
+        /// <param name="data">Element to add.</param>
+        public void AddElement(List<string> data)
+        {
+            var newElement = new Element(data, null);
+
+            if (IsEmpty())
+            {
+                head = newElement;
+                ++Size;
+                return;
+            }
+
+            Element nextElement = head;
+            Element previousElement = null;
+
+            while (nextElement != null && comparer.Compare(newElement.Data, nextElement.Data) == 1)
+            {
+                ++nextElement.Position;
+                --newElement.Position;
+                previousElement = nextElement;
+                nextElement = previousElement.Next;
+            }
+            if (nextElement != head)
+            {
+                newElement.Next = nextElement;
+                previousElement.Next = newElement;
+            }
+            else
+            {
+                newElement.Next = head;
+                head = newElement;
+            }
+            ++Size;
+        }
+
+        /// <summary>
+        /// Prints sorted set.
+        /// </summary>
+        public void PrintSet()
+        {
+            if (!IsEmpty())
+            {
+                var elementToPrint = head;
+
+                while (elementToPrint != null)
+                {
+                    for (var i = 0; i < elementToPrint.Data.Count; ++i)
+                    {
+                        Console.Write($"{elementToPrint.Data[i]} ");
+                    }
+                    Console.WriteLine();
+                    elementToPrint = elementToPrint.Next;
+                }
+            }
+        }
+    }
+}
