628ed152135c3a7e37d58c5ae9ef1dfdbe8d7aae
NadiaSob <sobo_na@mail.ru>
2019-05-21 19:56:27 +0300

    Added some comments [ci skip]

--- sem2/hw7/hw7.1/Calculator/Calculator/Calculator.cs
+++ sem2/hw7/hw7.1/Calculator/Calculator/Calculator.cs
@@ -2,8 +2,18 @@
 
 namespace homework
 {
+    /// <summary>
+    /// Class that do calculating operation with two numbers. 
+    /// </summary>
     public class Calculator
     {
+        /// <summary>
+        /// Do the operation with two numbers.
+        /// </summary>
+        /// <param name="operation">Symbol of the operation.</param>
+        /// <param name="firstNumber">First number of the expression.</param>
+        /// <param name="secondNumber">Second number of the expression.</param>
+        /// <returns>Result of the operation.</returns>
         public double Calculate(string operation, double firstNumber, double secondNumber)
         {
             switch (operation)
