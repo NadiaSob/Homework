56f26134328792f3ca31d35ba2e9cebe788b4a73
NadiaSob <sobo_na@mail.ru>
2019-05-25 15:22:00 +0300

    Added task 7.2

---
+++ sem2/hw7/hw7.2/Clock/Clock/ClockForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Clock
+{
+    partial class Clock
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.weekdayLabel = new System.Windows.Forms.Label();
+            this.dataLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.tableLayoutPanel.ColumnCount = 2;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 47F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 53F));
+            this.tableLayoutPanel.Controls.Add(this.timeLabel, 0, 0);
+            this.tableLayoutPanel.Controls.Add(this.weekdayLabel, 1, 0);
+            this.tableLayoutPanel.Controls.Add(this.dataLabel, 1, 1);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 2;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(509, 113);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.timeLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.timeLabel.Font = new System.Drawing.Font("Century Gothic", 28.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.timeLabel.ForeColor = System.Drawing.SystemColors.Info;
+            this.timeLabel.Location = new System.Drawing.Point(3, 0);
+            this.timeLabel.Name = "timeLabel";
+            this.tableLayoutPanel.SetRowSpan(this.timeLabel, 2);
+            this.timeLabel.Size = new System.Drawing.Size(233, 113);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "00:00:00";
+            this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // weekdayLabel
+            // 
+            this.weekdayLabel.AutoSize = true;
+            this.weekdayLabel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.weekdayLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.weekdayLabel.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.weekdayLabel.ForeColor = System.Drawing.SystemColors.Info;
+            this.weekdayLabel.Location = new System.Drawing.Point(242, 0);
+            this.weekdayLabel.Name = "weekdayLabel";
+            this.weekdayLabel.Size = new System.Drawing.Size(264, 56);
+            this.weekdayLabel.TabIndex = 1;
+            this.weekdayLabel.Text = "понедельник";
+            this.weekdayLabel.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // dataLabel
+            // 
+            this.dataLabel.AutoSize = true;
+            this.dataLabel.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.dataLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataLabel.Font = new System.Drawing.Font("Century Gothic", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dataLabel.ForeColor = System.Drawing.SystemColors.Info;
+            this.dataLabel.Location = new System.Drawing.Point(242, 56);
+            this.dataLabel.Name = "dataLabel";
+            this.dataLabel.Size = new System.Drawing.Size(264, 57);
+            this.dataLabel.TabIndex = 2;
+            this.dataLabel.Text = "1 января";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.OnTimerTick);
+            // 
+            // Clock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 113);
+            this.Controls.Add(this.tableLayoutPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(490, 160);
+            this.Name = "Clock";
+            this.Text = "Clock";
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label weekdayLabel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label dataLabel;
+    }
+}
+
---
+++ sem2/hw7/hw7.2/Clock/Clock/ClockForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Clock
+{
+    public partial class Clock : Form
+    {
+        public Clock()
+        {
+            InitializeComponent();
+        }
+
+        private void OnTimerTick(object sender, EventArgs e)
+        {
+            timeLabel.Text = DateTime.Now.ToString("HH:mm:ss");
+            dataLabel.Text = DateTime.Now.ToString("dd MMMM");
+            weekdayLabel.Text = DateTime.Now.ToString("dddd");
+        }
+    }
+}
